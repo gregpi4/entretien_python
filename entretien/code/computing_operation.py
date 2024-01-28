2fcfb7b5f9c8bb053b137dc1ce8878d4a92ad4d5
Bracewind <greg.pichereau@gmail.com>
2024-01-28 16:55:31 +0100

    add explanation

--- entretien/code/computing_operation.py
+++ entretien/code/computing_operation.py
@@ -4,6 +4,25 @@ from entretien.entretien_core.computing_operation import (
     OperationType,
 )
 
+"""
+the stream operation stream is an iterable of operations to execute
+it contains the number of line inside the stream and can be iterated
+to get the operations to execute.
+The possible operation are enumerated in OperationType.
+The operation can be VALUE, ADD, MULTIPLE, SUBSTRACT.
+Each argument of the operation is a string, and can take 3 types of values:
+- a number (ex: "1")
+- a reference to a previous operation result (ex: "$1")
+- nothing (ex: "_")
+The result of each operation must be given in the order the operations were given.
+This can be done by calling the add_line_answer method of the stream.
+
+Example:
+The solution for ["VALUE 1 _", "VALUE 3 _", "ADD 1 1"] is [1, 3, 2]
+The solution for ["VALUE 1 _", "VALUE 3 _", "ADD $1 0"] is [1, 3, 1]
+The solution for ["VALUE 1 _", "VALUE 3 _", "ADD $2 0"] is [1, 3, 3]
+
+"""
 
 def main(stream: OperationStream) -> None:
     nb_line = stream.number_of_operations
